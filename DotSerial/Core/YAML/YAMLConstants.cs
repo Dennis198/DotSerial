fddd6369bd627fb2f0c0fd399b12b32f31eeda88
Dennis Soelch <dennis.soelch@gmx.net>
2025-10-25 10:02:08 +0200

    Nachtrag

--- DotSerial/Core/YAML/YAMLConstants.cs
+++ DotSerial/Core/YAML/YAMLConstants.cs
@@ -1,3 +1,25 @@
+#region License
+//Copyright (c) 2025 Dennis Sölch
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+#endregion
+
 namespace DotSerial.Core.YAML
 {
     internal static class YAMLConstants
@@ -21,10 +43,10 @@ namespace DotSerial.Core.YAML
         /// <summary>
         /// End string of yaml document
         /// </summary>
-        internal const char KeyValueSeperator = ':';          
+        internal const char KeyValueSeperator = ':';
         /// <summary>
         /// Json null string
         /// </summary>
-        internal const string NullListItem = "- null";   
+        internal const string NullListItem = "- null";
     }
 }
