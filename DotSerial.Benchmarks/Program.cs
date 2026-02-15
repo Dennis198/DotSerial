55a0cbbd14dc8a9b0a78677d678a469e47030f36
Dennis Soelch <dennis.soelch@gmx.net>
2026-02-15 14:34:42 +0100

    Init

---
+++ DotSerial.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace DotSerial.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            //BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+        }
+    }
+}
